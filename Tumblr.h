443bfd5d7e93f606bfe80f125a05f1fca3ac6999
mcha8178 <chavez.mateo116@gmail.com>
2015-11-18 19:50:48 -0700

    Function Libraries
    We need a function library to store our ints and voids. It makes the main code look nicer.

---
+++ Tumblr.h
@@ -0,0 +1,101 @@
+int yas=1;
+int nah=0;
+int life=42;
+int PWNED=101;
+int killStreak=0;
+int THC=420;
+
+void noob()
+{
+	motor[cri]=100;						//lil twat
+}
+void noScope()
+{
+	if(SensorValue(scope)==0)
+		{
+			motor[motorA]=1337;
+			motor[motorB]=-1337;
+			wait1Msec(360);
+			motor[ballista]=-PWNED;
+			wait1Msec(life);
+			killStreak=killStreak+1;
+		}
+	else
+		{
+			noob();
+		}
+}
+void teaBag()
+{
+	motor[motorA]=117;
+	motor[motorB]=117;
+	wait10Msec(PWNED);
+	motor[motorA]=-117;
+	motor[motorB]=-117;
+	wait10Msec(PWNED);
+	motor[motorA]=117;
+	motor[motorB]=117;
+	wait10Msec(PWNED);
+	motor[motorA]=-117;
+	motor[motorB]=-117;
+	wait1Msec(PWNED);
+	motor[motorA]=117;
+	motor[motorB]=117;
+	wait10Msec(PWNED);
+	motor[motorA]=-117;
+	motor[motorB]=-117;
+	wait10Msec(PWNED);
+}
+void reevaluatePriorities()
+{
+	motor[cri]=100;
+}
+void wow()
+{
+/*shibe pleesed with humain
+─────────▄──────────────▄
+────────▌▒█───────────▄▀▒▌
+────────▌▒▒▀▄───────▄▀▒▒▒▐
+───────▐▄▀▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐
+─────▄▄▀▒▒▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐
+───▄▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▀██▀▒▌
+──▐▒▒▒▄▄▄▒▒▒▒▒▒▒▒▒▒▒▒▒▀▄▒▒▌
+──▌▒▒▐▄█▀▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐
+─▐▒▒▒▒▒▒▒▒▒▒▒▌██▀▒▒▒▒▒▒▒▒▀▄▌
+─▌▒▀▄██▄▒▒▒▒▒▒▒▒▒▒▒░░░░▒▒▒▒▌
+─▌▀▐▄█▄█▌▄▒▀▒▒▒▒▒▒░░░░░░▒▒▒▐
+▐▒▀▐▀▐▀▒▒▄▄▒▄▒▒▒▒▒░░░░░░▒▒▒▒▌
+▐▒▒▒▀▀▄▄▒▒▒▄▒▒▒▒▒▒░░░░░░▒▒▒▐
+─▌▒▒▒▒▒▒▀▀▀▒▒▒▒▒▒▒▒░░░░▒▒▒▒▌
+─▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐
+──▀▄▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▄▒▒▒▒▌
+────▀▄▒▒▒▒▒▒▒▒▒▒▄▄▄▀▒▒▒▒▄▀
+───▐▀▒▀▄▄▄▄▄▄▀▀▀▒▒▒▒▒▄▄▀
+──▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▀▀
+so doge    very dank    wow*/
+}
+void BASH()
+{
+	motor[motorA]=134;
+	motor[motorB]=-43;
+	wait10Msec(PWNED);
+	motor[motorA]=-72;
+	motor[motorB]=121;
+	wait1Msec(PWNED);
+	motor[motorA]=127;
+	motor[motorB]=-55;
+	wait10Msec(PWNED);
+	motor[motorA]=-94;
+	motor[motorB]=-14;
+	wait1Msec(PWNED);
+	motor[motorA]=77;
+	motor[motorB]=-36;
+	wait10Msec(PWNED);
+	motor[motorA]=-83;
+	motor[motorB]=124;
+	wait1Msec(PWNED);
+}
+void UWOTM8()
+{
+	BASH();								//SHIBE DISPLESSD
+}
